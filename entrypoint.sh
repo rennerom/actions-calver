7f58830ce17b8520839ace1588411e2064f750c7
Max Renner <rennerom@gmail.com>
2022-10-06 12:40:20 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ if [ "${NAME}" = "0" ]; then
 fi
 
 if [ "${MESSAGE}" = "0" ]; then
-  MESSAGE=$(conventional-changelog)
+  MESSAGE=$(conventional-changelog -p angular)
 fi
 
 echo "Next release : ${NEXT_RELEASE}"
